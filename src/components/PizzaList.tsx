e5ddb8d57645d5ab7962049d6a686378b8472272
Gary Poster <gary@modernsongs.com>
2020-09-26 16:33:03 -0400

    Style pizza list

--- src/components/PizzaList.tsx
+++ src/components/PizzaList.tsx
@@ -1,6 +1,28 @@
 import { Link } from "gatsby";
 import React from "react";
 import Img from "gatsby-image";
+import styled from "styled-components";
+
+const PizzaGridStyles = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  gap: 4rem;
+  grid-auto-rows: auto auto 500px;
+`;
+
+const PizzaStyles = styled.div`
+  display: grid;
+  @supports not (grid-template-rows: subgrid) {
+    grid-template-rows: auto auto 1fr;
+  }
+  grid-template-rows: var(--rows, subgrid);
+  grid-row: span 3;
+  grid-gap: 1rem;
+  h2,
+  p {
+    margin: 0;
+  }
+`;
 
 type Pizzas = GatsbyTypes.PizzasQuery["pizzas"]["nodes"];
 
@@ -13,13 +35,15 @@ interface PizzaProps {
 function Pizza({ pizza }: PizzaProps): JSX.Element {
   const imgData = pizza.image?.asset?.fluid;
   return (
-    <Link to={`/pizza/${pizza.slug?.current}`}>
-      <h2>
-        <span className="mark">{pizza.name}</span>
-      </h2>
+    <PizzaStyles>
+      <Link to={`/pizza/${pizza.slug?.current}`}>
+        <h2>
+          <span className="mark">{pizza.name}</span>
+        </h2>
+      </Link>
       <p>{pizza.toppings?.map((topping) => topping?.name).join(", ")}</p>
       {imgData ? <Img fluid={imgData} alt={pizza.name} /> : ""}
-    </Link>
+    </PizzaStyles>
   );
 }
 
@@ -29,10 +53,10 @@ interface PizzaListProps {
 
 export default function PizzaList({ pizzas }: PizzaListProps): JSX.Element {
   return (
-    <>
+    <PizzaGridStyles>
       {pizzas.map((pizza) => (
         <Pizza key={pizza.id} pizza={pizza} />
       ))}
-    </>
+    </PizzaGridStyles>
   );
 }
